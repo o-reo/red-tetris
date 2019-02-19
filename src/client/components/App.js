e5361e0a4938235ad240d171600dff86e7203a0e
befuhro <befuhro@student.le-101.fr>
2019-02-19 13:36:41 +0100

    modif background + hash based url

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import WebFont from 'webfontloader';
 import Game from '../components/Game';
 import Footer from '../components/Footer';
+
+WebFont.load({
+    google: {
+        families: ["Permanent Marker"]
+    }
+});
 
 const appStyle = {
     height: "100%",
@@ -11,11 +18,47 @@ const appStyle = {
     alignItems: "center",
 };
 
-const App = () => (
-<div style={appStyle}>
-    <Game/>
-    <Footer/>
-</div>
-);
+const errorMessage = {
+    width: "35vh",
+    height: "10vh",
+    margin: "32.9vh",
+    padding: "5vh",
+    backgroundColor: '#0b1d2d',
+    color: "#61dafb",
+    fontFamily: "Permanent Marker",
+    fontSize: "3vh",
+    fontWeight: "300",
+    textAlign: "center",
+};
+
+function redirectToRoom(urlParams)
+{
+    if (urlParams.room === "room" && urlParams.player === "player") {
+        return (true);
+    }
+    else {
+        return (false);
+    }
+}
+
+const App = ({match}) => {
+
+    if (redirectToRoom(match.params) === true) {
+        return (
+            <div style={appStyle}>
+                <Game/>
+                <Footer/>
+            </div>
+        )
+    }
+    else {
+        return (
+            <div style={appStyle}>
+                <p style={errorMessage}>You are not authentificated!</p>
+                <Footer/>
+            </div>
+        )
+    }
+};
 
 export default connect()(App);
--- src/client/index.js
+++ src/client/index.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import { createStore } from 'redux';
 import rootReducer from './reducers/index.js';
 import App from './containers/App.js';
+import CreateRoom from './components/CreateRoom';
 import './assets/style.css';
 
 const store = createStore(rootReducer);
 
 render(
-   <Provider store={store}>
-        <App />
-    </Provider>,
+    <Provider store={store}>
+        <HashRouter>
+            <Switch>
+                <Route path="/:room[:player]" component={ App }/>
+                <Route path="/" component={ CreateRoom }/>
+            </Switch>
+        </HashRouter>
+    </Provider>
+    ,
     document.getElementById('root')
 );
